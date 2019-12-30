0c7a237c248658664779f40b008d560d6cf107d6
yyzhouzy <764646262@qq.com>
2019-12-30 15:03:21 +0800

    add client

---
+++ client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+  import {mapActions} from 'vuex'
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+  export default {
+
+    mounted () {
+      // this.$store.dispatch('getAddress')
+      this.getAddress()
+      this.getUserInfo()
+    },
+
+    methods: {
+      ...mapActions(['getAddress', 'getUserInfo'])
+    },
+
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixins.styl"
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
